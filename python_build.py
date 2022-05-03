fc69339fdfe27c421b3064b904530bf0ac273165
jnapolitano <justin@jnapolitano.io>
2022-05-03 15:15:52 -0500

    autocommit on1651608952.503919

--- python_build.py
+++ python_build.py
@@ -140,7 +140,7 @@ class deploy_pipeline:
 
 
         #subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'my_package'])
-        result = subprocess.run(['ghp-import', '-n', '-p', '-f', '-c', cname, '_build/html' ], capture_output=True, text=True)
+        result = subprocess.run(['ghp-import', '-n', '-p', '-f', '-c', cname, 'jupyter-book/_build/html' ], capture_output=True, text=True)
         ##print("Output")
 
         print(result.stdout)
